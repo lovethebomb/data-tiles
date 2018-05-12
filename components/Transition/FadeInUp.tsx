605d9c5e3e64e360ec79ca66058b32a683daf2bd
lovethebomb <lucas@lovethebomb.eu>
2018-05-12 20:57:31 +0200

    Add opiniated and basic FadeInUp transition

---
+++ components/Transition/FadeInUp.tsx
@@ -0,0 +1,50 @@
+import React from 'react';
+import { Transition } from 'react-transition-group'
+
+const FadeIn = ({ children, duration = 500, up = "80px", delay = 0, easing = "ease", visible = false }) => {
+    const styles = {
+        entered: {
+            opacity: 1,
+            transform: 'translate3d(0, 0, 0)'
+        },
+        entering: {
+            opacity: 0.01,
+            transform: `translate3d(0, ${up}, 0)`
+        },
+        exited: {
+            opacity: 0.01,
+            transform: `translate3d(0, ${up}, 0)`
+        },
+        exiting: {
+            opacity: 0.01,
+            transform: `translate3d(0, ${up}, 0)`
+        }
+    };
+
+    const renderChildren = (state)  => {
+        return React.Children.map(children, child => {
+            return React.cloneElement(child, {
+                style: {
+                    transitionDelay: [`${delay}ms`, `${delay + 10}ms`],
+                    transitionDuration: [`${duration}ms`, `${duration}ms`],
+                    transitionProperty: ['opacity', 'transform'],
+                    transitionTimingFunction: easing,
+                    willChange: 'opacity',
+                    ...styles[state]
+                }
+            })
+        })
+    }
+
+    return (
+        <Transition in={visible} timeout={delay + duration}>
+        {(state) => (
+            <React.Fragment>
+                {renderChildren(state)}
+            </React.Fragment>
+        )}
+        </Transition>
+    )
+}
+
+export default FadeIn;
