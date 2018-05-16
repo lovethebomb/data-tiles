3ca06023ec482b07d0333dbab64501503d61f629
lovethebomb <lucas@lovethebomb.eu>
2018-05-16 21:42:39 +0200

    Update README, fix typo in about page

--- README.md
+++ README.md
@@ -1,9 +1,25 @@
 # Data Tiles
 
-TODO.
+This application is a simple side-project created to learn a bit more about [Next.js](https://github.com/zeit/next.js/) and [TypeScript]("https://www.typescriptlang.org").
 
 ## Usage
 
 ```bash
 git clone git@github.com:lovethebomb/data-tiles.git
+```
+
+Create a `.env` file with `KEY=value` as decribed in `.config.js`
+
+## Development
+
+```bash
+npm run dev
+```
+
+## Production
+
+A Dockerfile is provided, but you can also juste run:
+
+```bash
+npm run build && npm run start
 ```
--- pages/about.tsx
+++ pages/about.tsx
@@ -9,7 +9,7 @@ const About: StatelessComponent<any> = () => (
   <Layout>
       <Header />
       <div className="About">
-            <p>This application is a sample side-project created to learn a bit more about <a href="https://github.com/zeit/next.js/">Next.js</a> and <a href="https://www.typescriptlang.org/">TypeScript</a>.</p>
+            <p>This application is a simple side-project created to learn a bit more about <a href="https://github.com/zeit/next.js/">Next.js</a> and <a href="https://www.typescriptlang.org/">TypeScript</a>.</p>
             <p>All the API requests are handled by a small express project, which also serve the <a href="https://github.com/zeit/next.js/">Next.js</a> app.</p>
             <p>Feel free to checkout the code on <a href="https://github.com/lovethebomb/data-tiles">Github</a> or <a href="https://www.lucas.computer">reach out</a> to me!</p>
       </div>
