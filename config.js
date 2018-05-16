6bbca19f93ef6728e767e8f6277ac817feadda26
lovethebomb <lucas@lovethebomb.eu>
2018-05-16 15:22:20 +0200

    Update packages, add dotenv config

---
+++ config.js
@@ -0,0 +1,16 @@
+require('dotenv').config()
+
+module.exports = {
+    LASTFM_USERNAME: process.env.LASTFM_USERNAME,
+    LASTFM_API_KEY: process.env.LASTFM_API_KEY,
+    PUBG_API_KEY: process.env.PUBG_API_KEY,
+    PUBG_PLAYER_ID: process.env.PUBG_PLAYER_ID,
+    DISCOGS_USERNAME: process.env.DISCOGS_USERNAME,
+    DISCOGS_API_KEY: process.env.DISCOGS_API_KEY,
+    TWITTER_USERNAME: process.env.TWITTER_USERNAME,
+    GITHUB_USERNAME: process.env.GITHUB_USERNAME,
+    GITHUB_API_KEY: process.env.GITHUB_API_KEY,
+    BATTLE_NET_USER: process.env.BATTLE_NET_USER,
+    SPOTIFY_CLIENT_ID: process.env.SPOTIFY_CLIENT_ID,
+    SPOTIFY_CLIENT_SECRET: process.env.SPOTIFY_CLIENT_SECRET,
+};
