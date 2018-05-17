c649fd8bd2ede816f1d91dc20ffe661ec4c98437
lovethebomb <lucas@lovethebomb.eu>
2018-05-17 02:36:05 +0200

    Improve Layout head with more meta

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -4,9 +4,18 @@ import css from 'styled-jsx/css'
 const Layout = (props) => (
   <section>
     <Head>
-      <title>Data Tiles</title>
-      <meta name="viewport" content="initial-scale=1.0, width=device-width" />
-      <link href="https://fonts.googleapis.com/css?family=Rubik:300,400" rel="stylesheet" />
+      <title>Data Tiles - lucas.computer</title>
+      <meta name="viewport" content="initial-scale=1.0, width=device-width"/>
+      <meta name="theme-color" content="#111" />
+      <meta name="description" content="Simple tiles showing some unfamous statistics."/>
+      <meta name="robots" content="index,follow"/>
+      <meta name="twitter:creator" content="@ltb_lucas"/>
+      <link rel="dns-prefetch" href="//fonts.googleapis.com"/>
+      <link rel="dns-prefetch" href="//fonts.gstatic.com"/>
+      <meta property="og:type" content="website"/>
+      <meta property="og:author" content="Lucas Heymès, @ltb_lucas" />
+      <meta property="og:url" content="https://data.lucas.computer" />
+      <link href="https://fonts.googleapis.com/css?family=Rubik:300,400" rel="stylesheet"/>
     </Head>
     <article role="main">
       {props.children}
