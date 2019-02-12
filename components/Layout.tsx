8e1d849c9b28dc4f3fd418579e61e4ff8a47e89b
Lucas Heymès <lucas@lovethebomb.eu>
2019-02-12 01:05:14 +0100

    fix(layout): Fix global styling

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,5 +1,4 @@
 import Head from 'next/head'
-import css from 'styled-jsx/css'
 import Fathom from './Fathom'
 
 const Layout = (props) => (
@@ -21,90 +20,8 @@ const Layout = (props) => (
     </Head>
     <article role="main">
       {props.children}
-      <style jsx={true} global={true}>{layoutStyle}</style>
     </article>
   </section>
 )
-
-const layoutStyle = css`
-html, body {
-  width: 100%;
-  height: 100%;
-  background-color: #181818;
-  font-family: -apple-system,BlinkMacSystemFont,"Segoe UI",Helvetica,Arial,sans-serif,"Apple Color Emoji","Segoe UI Emoji","Segoe UI Symbol";
-  font-size: 16px;
-  line-height: 1.2em;
-  letter-spacing: 0.05em;
-  color: #FFF8EA;
-  font-weight: 300;
-  margin: 0;
-}
-
-section {
-  padding: 2em;
-}
-
-article {
-}
-
-article p {
-  margin: auto;
-  line-height: 1.6em;
-  margin: 1em auto;
-}
-
-h1 {
-  font-weight: 700;
-  margin: auto;
-  font-size: 32px;
-  margin: 0.5em auto auto;
-}
-
-h2 {
-  font-size: 20px;
-  font-weight: 300;
-  margin: 0.2em auto 0.5em auto;
-}
-
-h3 {
-  font-size: 14px;
-  font-weight: 300;
-  text-transform: uppercase;
-  letter-spacing: 0.1em;
-}
-
-aside {
-  margin-top: 2em;
-}
-
-ul {
-  margin: 0;
-  padding: 0;
-  list-style: none;
-}
-
-li {
-  margin: .5em auto;
-}
-
-a {
-  color: inherit;
-  font-weight: 400;
-}
-
-a:hover {
-  color: #b5b5b5;
-}
-
-@media screen and (max-width: 320px) {
-  section {
-    padding: 10px;
-  }
-
-  .Tiles .Tile {
-    margin: 0.5em 0;
-  } 
-}
-`;
 
 export default Layout
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,9 @@
-import React from 'react'
 import App, { Container } from 'next/app'
+import React from 'react'
+import css from 'styled-jsx/css'
 
 export default class MyApp extends App {
-    static async getInitialProps({ Component, router, ctx }) {
+   public static async getInitialProps({ Component, router, ctx }) {
         let pageProps = {}
 
         if (Component.getInitialProps) {
@@ -12,7 +13,7 @@ export default class MyApp extends App {
         return { pageProps }
     }
 
-    render() {
+    public render() {
         if (process.browser && window.fathom) {
             window.fathom('trackPageview');
         }
@@ -21,7 +22,89 @@ export default class MyApp extends App {
         return (
             <Container>
                 <Component {...pageProps} />
+                <style jsx={true} global={true}>{layoutStyle}</style>
             </Container>
         )
     }
 }
+
+const layoutStyle = css.global`
+html, body {
+  width: 100%;
+  height: 100%;
+  background-color: #181818;
+  font-family: -apple-system,BlinkMacSystemFont,"Segoe UI",Helvetica,Arial,sans-serif,"Apple Color Emoji","Segoe UI Emoji","Segoe UI Symbol";
+  font-size: 16px;
+  line-height: 1.2em;
+  letter-spacing: 0.05em;
+  color: #FFF8EA;
+  font-weight: 300;
+  margin: 0;
+}
+
+section {
+  padding: 2em;
+}
+
+article {
+}
+
+article p {
+  margin: auto;
+  line-height: 1.6em;
+  margin: 1em auto;
+}
+
+h1 {
+  font-weight: 700;
+  margin: auto;
+  font-size: 32px;
+  margin: 0.5em auto auto;
+}
+
+h2 {
+  font-size: 20px;
+  font-weight: 300;
+  margin: 0.2em auto 0.5em auto;
+}
+
+h3 {
+  font-size: 14px;
+  font-weight: 300;
+  text-transform: uppercase;
+  letter-spacing: 0.1em;
+}
+
+aside {
+  margin-top: 2em;
+}
+
+ul {
+  margin: 0;
+  padding: 0;
+  list-style: none;
+}
+
+li {
+  margin: .5em auto;
+}
+
+a {
+  color: inherit;
+  font-weight: 400;
+}
+
+a:hover {
+  color: #b5b5b5;
+}
+
+@media screen and (max-width: 320px) {
+  section {
+    padding: 10px;
+  }
+
+  .Tiles .Tile {
+    margin: 0.5em 0;
+  } 
+}
+`;
